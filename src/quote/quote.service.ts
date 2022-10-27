6bf9959dfded354560c9f17e8a71d31d06471ee0
jmamadeu <jmamadeu2000@gmail.com>
2022-10-27 23:21:07 +0100

    chore: add prisma

--- src/quote/quote.service.ts
+++ src/quote/quote.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class QuoteService {
+  constructor(private prisma: PrismaService) {}
+
   findAll(): string[] {
     return ['Quote 1', 'Quote 2', 'Quote 4'];
   }
